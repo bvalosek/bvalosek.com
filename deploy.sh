6dde969d35b065cb5aa42c26139ed9e0a70a3357
Brandon Valosek <bvalosek@gmail.com>
2016-01-04 11:28:37 -0600

    circle

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# update hub
-# https://hub.docker.com/r/bvalosek/bvalosek.com/
-docker build -t bvalosek/bvalosek.com:latest .
-docker push bvalosek/bvalosek.com:latest
-
-# boot it
-docker-compose up -d
